f6371843cd7c1da566cbe98d6855ca0aa0304622
Natalia Maniakowska <natalia.maniakowska@skygate.io>
2018-12-17 00:22:34 +0100

    [2018] Day 4, part 2 - faster solution
    Tried to use some neat numpy tricks with arrays, but
    it's not so neat after all. This solution is much
    faster, though!

--- aoc_04_guards.py
+++ aoc_04_guards.py
@@ -1,9 +1,6 @@
 import re
-
+import timeit
 import numpy as np
-
-with open('04_input', 'r') as f:
-    data = f.read()
 
 shift_start_p = re.compile(r'\[\d{4}-\d{2}-\d{2} \d{2}:(?P<minutes>\d{2})\] Guard #(?P<id>\d+) begins shift')
 falls_asleep_p = re.compile(r'\[\d{4}-\d{2}-\d{2} \d{2}:(?P<minutes>\d{2})\] falls asleep')
@@ -58,7 +55,7 @@ def solve_part_one(data):
     return int(sleepy_guard_id) * sleepy_minute
 
 
-class SolvesPartTwo:
+class SolvesPartTwoVersion1:
     def __init__(self, data):
         self.chrono_records = sorted(data.splitlines())
         self.nap_times = calculate_guards_nap_times(self.chrono_records)
@@ -83,9 +80,66 @@ class SolvesPartTwo:
         return guard_data[1][1]
 
 
+class SolvesPartTwoVersion2:
+    """
+    This solution seems to be significantly faster, even if solve() is rather messy...
+    """
+
+    def __init__(self, data):
+        self.chrono_records = sorted(data.splitlines())
+        self.nap_times = calculate_guards_nap_times(self.chrono_records)
+        guard_ids = set(self.nap_times.keys())
+        self.minutes_table = np.zeros((len(guard_ids), 60), np.int8)
+        self.guard_ids = {guard_id: array_index for array_index, guard_id in enumerate(guard_ids)}
+
+    def solve(self):
+        self.fill_minutes_table()
+
+        sleepy_minutes = self.minutes_table.argmax(axis=1)
+        sleep_counts = np.amax(self.minutes_table, axis=1)
+
+        minutes_and_counts = np.hstack((sleepy_minutes.reshape((-1, 1)), sleep_counts.reshape((-1, 1))))
+
+        max_minute_number = max(minutes_and_counts, key=lambda t: t[1])[0]
+        max_index = minutes_and_counts.argmax(axis=0)[1]
+
+        guard_id = list(self.guard_ids.keys())[list(self.guard_ids.values()).index(max_index)]
+
+        return guard_id * max_minute_number
+        ## ok, lol, whatever xD but this version works too.
+
+    def fill_minutes_table(self):
+        """
+        Fill the self.minutes_table so that each row contains the 60 minutes
+        during each guard's shifts and the numbers in each minute are counts
+        of times when given guard was asleep during this minute.
+
+        :return: None
+        """
+
+        for record in self.chrono_records:
+            if shift_start_p.match(record):
+                guard_id = int(shift_start_p.match(record).group('id'))
+            elif falls_asleep_p.match(record):
+                falls_asleep_minute = int(falls_asleep_p.match(record).group('minutes'))
+            elif wakes_up_p.match(record):
+                wakes_up_minute = int(wakes_up_p.match(record).group('minutes'))
+                self.minutes_table[self.guard_ids[guard_id]][falls_asleep_minute:wakes_up_minute] += 1
+
+    @staticmethod
+    def _get_times_asleep(guard_data):
+        return guard_data[1][1]
+
+
 if __name__ == '__main__':
     with open('04_input', 'r') as f:
         data = f.read()
 
-    # print(solve_part_one(data))
-    print(SolvesPartTwo(data).solve())
+    print(solve_part_one(data))
+    print(SolvesPartTwoVersion2(data).solve())
+    # print('Part 2, ver. 1: ',
+    #       timeit.timeit('SolvesPartTwoVersion1(data).solve()', number=100, globals=globals()))
+    #       # 4.881550515005074 s
+    # print('Part 2, ver. 2: ',
+    #       timeit.timeit('SolvesPartTwoVersion2(data).solve()', number=100, globals=globals()))
+    #       # 0.5612120069999946 s
