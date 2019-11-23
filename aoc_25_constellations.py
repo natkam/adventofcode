d66a3bef0e2ef6ccd437489c14fcf6f3aeaf0a78
Natalia Maniakowska <natalia.maniakowska@skygate.io>
2019-11-23 21:03:42 +0100

    [2018] Day 25, pt.1 - add comments, run black

--- aoc_25_constellations.py
+++ aoc_25_constellations.py
@@ -1,10 +1,10 @@
 import operator
 from functools import reduce
 
-with open('25_input', 'r') as f:
+with open("25_input", "r") as f:
     data = f.read()
 
-points = sorted(data.splitlines())
+points = sorted(set(data.splitlines()))  # duplicates can be removed
 MAX_DISTANCE = 3
 
 
@@ -13,16 +13,19 @@ def calculate_distance(point_1, point_2):
 
 
 def belongs_to_constellation(point, constellation):
-    return any((calculate_distance(point, star) <= MAX_DISTANCE for star in constellation))
+    return any(
+        (calculate_distance(point, star) <= MAX_DISTANCE for star in constellation)
+    )
 
 
 def solve_part_one(points):
-    # TODO: refactor to make more readable
     constellations = []
 
     for point in points:
-        point = [int(coord) for coord in point.split(',')]
+        point = [int(coord) for coord in point.split(",")]
 
+        # All the constellations to which the point belongs (i.e. which are connected
+        # into one by this point) have to be merged into one constellation afterwards
         constellations_to_merge = []
         point_belongs_somewhere = False
 
@@ -30,12 +33,14 @@ def solve_part_one(points):
             if belongs_to_constellation(point, constellation):
                 point_belongs_somewhere = True
                 if not constellations_to_merge:
-                    constellation.append(point)
+                    constellation.append(point)  # only add point to one constellation
 
                 constellations_to_merge.append(constellation)
 
         if constellations_to_merge:
-            constellations = [c for c in constellations if c not in constellations_to_merge]
+            constellations = [
+                c for c in constellations if c not in constellations_to_merge
+            ]
             constellations.append(reduce(operator.add, constellations_to_merge))
 
         if not point_belongs_somewhere:
@@ -44,5 +49,5 @@ def solve_part_one(points):
     return len(constellations)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(solve_part_one(points))
