5308426fb9ccd702acf66119c7f0ef690f6d3bca
Natalia Maniakowska <natalia.maniakowska@skygate.io>
2019-11-25 00:44:17 +0100

    [2018] Day 6: fix a typo in pt. 1, solve pt. 2
    Incidentally, the typo in the 1st part didn't make the solution wrong.
    But it slowed it down, as a bigger array has to be calculated now
    (apparently, the array could be trimmed...)

--- 2018/aoc_06_chrono_coords.py
+++ 2018/aoc_06_chrono_coords.py
@@ -1,3 +1,4 @@
+import typing
 from collections import Counter
 
 import numpy as np
@@ -13,12 +14,26 @@ COORDS = {
 }
 
 
+def calculate_extrema():
+    x_coords = set(pair[0] for pair in COORDS.values())
+    y_coords = set(pair[1] for pair in COORDS.values())
+    x_min, x_max = min(x_coords), max(x_coords)
+    y_min, y_max = min(y_coords), max(y_coords)
+
+    return x_min, x_max, y_min, y_max
+
+
 def calculate_distance(point_1, point_2):
     return sum((abs(coord_1 - coord_2) for coord_1, coord_2 in zip(point_1, point_2)))
 
 
+def get_distances_list(x: int, y: int) -> typing.List:
+    # TODO: these get calculated for each part of the task; store them somewhere?
+    return [(pt, calculate_distance((x, y), COORDS[pt])) for pt in COORDS]
+
+
 def get_closest_point(x: int, y: int) -> str:
-    distances = [(pt, calculate_distance((x, y), COORDS[pt])) for pt in COORDS]
+    distances = get_distances_list(x, y)
 
     # have to find the minimum manually, because ties don't count!
     closest_points = [distances[0][0]]
@@ -37,33 +52,56 @@ def get_closest_point(x: int, y: int) -> str:
     return closest_points[0]
 
 
-def solve_first():
-    x_coords = set(pair[0] for pair in COORDS.values())
-    y_coords = set(pair[1] for pair in COORDS.values())
-    x_min, x_max = min(x_coords), max(x_coords)
-    y_min, y_max = min(y_coords), max(y_coords)
-
+def get_grid_of_closest_points() -> np.ndarray:
+    x_min, x_max, y_min, y_max = calculate_extrema()
     values = [
-        [get_closest_point(x + x_min, y + y_min) for x in range(y_max + 1 - y_min)]
+        [get_closest_point(x + x_min, y + y_min) for x in range(x_max + 1 - x_min)]
         for y in range(y_max + 1 - y_min)
     ]
 
-    ar = np.array(values)
+    return np.array(values)
+
+
+def get_total_distance(x: int, y: int):
+    return sum(d[1] for d in get_distances_list(x, y))
+
+
+def get_grid_of_total_distances():
+    x_min, x_max, y_min, y_max = calculate_extrema()
+    values = [
+        [get_total_distance(x + x_min, y + y_min) for x in range(x_max + 1 - x_min)]
+        for y in range(y_max + 1 - y_min)
+    ]
+
+    return np.array(values)
+
+
+def solve_first():
+    grid = get_grid_of_closest_points()
     values_on_edges = set(
-        np.concatenate((np.unique(ar[[0, -1]]), np.unique(ar[:, [0, -1]])))
+        np.concatenate((np.unique(grid[[0, -1]]), np.unique(grid[:, [0, -1]])))
     )
 
-    c = Counter(ar.flatten())
+    c = Counter(grid.flatten())
     del c["x"]  # get rid of the ties
     for edge in values_on_edges:
         del c[edge]
-    result = c.most_common(1)
+    biggest_area: typing.Tuple[str, int] = c.most_common(1)[0]
+
+    return biggest_area[1]
+
+
+def solve_second():
+    grid = get_grid_of_total_distances()
+    result = grid[grid < 10000].shape[0]
 
     return result
 
+
 if __name__ == "__main__":
     import time
-    start = time.perf_counter()
-    print(solve_first())
-    print(time.perf_counter() - start)  # ~ 4.5 s
 
+    start = time.perf_counter()
+    print(solve_first())  # ~ 5 s; the calculated area could be smaller, in fact
+    print(solve_second())  # ~ 5 s
+    print(time.perf_counter() - start)  # ~ 10 s
