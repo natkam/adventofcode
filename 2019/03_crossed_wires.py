c0e13d80c02b0c9c3cf137710c18963a19dcc04d
Natalia Maniakowska <natalia.maniakowska@skygate.io>
2019-12-04 17:16:58 +0100

    [2019] Fix a minor bug in day 3, pt. 1 solution

--- 2019/03_crossed_wires.py
+++ 2019/03_crossed_wires.py
@@ -9,16 +9,16 @@ def get_wire_coords(wire_path: typing.List[str]) -> typing.Set:
         direction = path[0]
         steps_count = int(path[1:])
         if direction == "R":
-            coords.update([(x_now + x, y_now) for x in range(1, steps_count)])
+            coords.update([(x_now + x, y_now) for x in range(1, steps_count + 1)])
             x_now += steps_count
         elif direction == "L":
-            coords.update([(x_now - x, y_now) for x in range(1, steps_count)])
+            coords.update([(x_now - x, y_now) for x in range(1, steps_count + 1)])
             x_now -= steps_count
         elif direction == "U":
-            coords.update([(x_now, y_now + y) for y in range(1, steps_count)])
+            coords.update([(x_now, y_now + y) for y in range(1, steps_count + 1)])
             y_now += steps_count
         elif direction == "D":
-            coords.update([(x_now, y_now - y) for y in range(1, steps_count)])
+            coords.update([(x_now, y_now - y) for y in range(1, steps_count + 1)])
             y_now -= steps_count
 
     return coords
@@ -33,8 +33,8 @@ def solve_part_one(data):
     coords_1 = get_wire_coords(first_wire)
     coords_2 = get_wire_coords(second_wire)
 
-    crossed_points = coords_1.intersection(coords_2)
-    cross_distances = [calculate_distance((0, 0), point) for point in crossed_points]
+    cross_points = coords_1.intersection(coords_2)
+    cross_distances = [calculate_distance((0, 0), point) for point in cross_points]
 
     return min(cross_distances)
 
