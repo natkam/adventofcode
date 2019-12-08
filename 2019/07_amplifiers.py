5c4d512cea3a2bf5ada8e0f17dea51cb7cabd9dd
natkam <natka_m@o2.pl>
2019-12-08 05:52:25 +0100

    [2019/day7] [WIP, barely started] Part 2

--- 2019/07_amplifiers.py
+++ 2019/07_amplifiers.py
@@ -27,7 +27,35 @@ def solve_part_one():
     return max_signal
 
 
+def solve_part_two():
+    max_signal = 0
+
+    for input_set in permutations([5, 6, 7, 8, 9]):
+        with open("07_input") as f:
+            opcodes = [int(code) for code in f.read().split(",")]
+
+        # function that takes the previous instance and gets its output?
+        # or subprocesses/multiprocessing, or what?
+
+        amp_A = Computer(opcodes.copy(), input_set[0], 0)
+        amp_A.solve()
+        amp_B = Computer(opcodes.copy(), input_set[1], amp_A.next_input)
+        amp_B.solve()
+        amp_C = Computer(opcodes.copy(), input_set[2], amp_B.next_input)
+        amp_C.solve()
+        amp_D = Computer(opcodes.copy(), input_set[3], amp_C.next_input)
+        amp_D.solve()
+        amp_E = Computer(opcodes.copy(), input_set[4], amp_D.next_input)
+        amp_E.solve()
+
+        if amp_E.next_input > max_signal:
+            max_signal = amp_E.next_input
+
+    return max_signal
+
+
 if __name__ == "__main__":
-    with open("07_input") as f:
-        opcodes = [int(code) for code in f.read().split(",")]
-    print(solve_part_one())
+    # with open("07_input") as f:
+    # opcodes = [int(code) for code in f.read().split(",")]
+    # print(solve_part_one())
+    print(solve_part_two())
