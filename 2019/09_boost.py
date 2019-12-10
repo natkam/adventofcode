a7a53e6c50dacb904ea26949b3697d701f745109
natkam <natka_m@o2.pl>
2019-12-10 01:51:57 +0100

    [2019/d9] Start working on part 1 [WIP]

---
+++ 2019/09_boost.py
@@ -0,0 +1,35 @@
+from computer import Computer
+
+
+def test_cases_pt_1():
+    opcodes = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99".split(",")
+    opcodes = [int(code) for code in opcodes]
+    opcodes.extend(0 for _ in range(100))
+    c = Computer(opcodes)
+    c.solve()  # no input, outputs a copy of itself - i.e. self.opcodes? or stdout??
+    print(c.opcodes, "\n")
+
+    opcodes = [int(code) for code in "1102,34915192,34915192,7,4,7,99,0".split(",")]
+    c = Computer(opcodes)
+    c.solve()  # output a 16-digit number
+    print(c.opcodes, "\n")
+
+    opcodes = [int(code) for code in "104,1125899906842624,99".split(",")]
+    c = Computer(opcodes)
+    c.solve()  # output the large number in the middle
+    print(c.opcodes, "\n")
+
+
+if __name__ == "__main__":
+    test_cases_pt_1()
+
+    # TODO: handle mode 2 for "write" operations! - see opcode 3 method
+    # TODO 2: try to think of a better way to extend the "memory" (additional zeros)
+    # TODO 3: refactor the code methods - repeated code?
+
+    # with open("09_input", "r") as f:
+    #     data = f.read()
+    #
+    # opcodes = [int(code) for code in data.split(",")]
+    # opcodes.extend(0 for _ in range(1000))
+    # Computer(opcodes, initial_input=1).solve()
