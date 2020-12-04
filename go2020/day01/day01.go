37835a428870296e2a60ef0a45f82c4ae79de234
natkam <natka_m@o2.pl>
2020-12-05 00:28:11 +0100

    [2020/d.1] [Go] Solve part 2, with tests

--- go2020/day01/day01.go
+++ go2020/day01/day01.go
@@ -25,7 +25,33 @@ func solvePartOne(data []string) (int64, error) {
 			}
 		}
 	}
-	return 0, errors.New("No entries sum up to 2020!")
+	return 0, errors.New("no two entries sum up to 2020")
+}
+
+func solvePartTwo(data []string) (int64, error) {
+	for i := 0; i < len(data); i++ {
+		for j := i + 1; j < len(data); j++ {
+			for k := j + 1; k < len(data); k++ {
+				ith, err := strconv.ParseInt(data[i], 10, 64)
+				if err != nil {
+					log.Fatal(err)
+				}
+				jth, err := strconv.ParseInt(data[j], 10, 64)
+				if err != nil {
+					log.Fatal(err)
+				}
+				kth, err := strconv.ParseInt(data[k], 10, 64)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if ith+jth+kth == 2020 {
+					return ith * jth * kth, nil
+				}
+			}
+		}
+	}
+	return 0, errors.New("no three entries sum up to 2020")
 }
 
 func main() {
@@ -36,9 +62,16 @@ func main() {
 		log.Fatal(err)
 	}
 	data := strings.Fields(string(file))
-	result, err := solvePartOne(data)
+
+	result1, err := solvePartOne(data)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(result)
+	fmt.Println(result1)
+
+	result2, err := solvePartTwo(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(result2)
 }
--- go2020/day01/day01_test.go
+++ go2020/day01/day01_test.go
@@ -7,15 +7,31 @@ func TestSolvePartOne(t *testing.T) {
 	var want int64 = 514579
 	result, err := solvePartOne(input)
 	if want != result || err != nil {
-		t.Fatalf(`solvePartOne(input) = %v, %v; want %#q`, result, err, want)
+		t.Fatalf(`solvePartOne(input) = %v, %v; want %v.`, result, err, want)
 	}
 }
-
 
 func TestSolvePartOneWithoutResult(t *testing.T) {
 	input := []string{"1720", "979", "366", "299", "675", "1456"}
 	result, err := solvePartOne(input)
 	if result != 0 || err == nil {
-		t.Fatalf(`solvePartOne(input) = %v, %v; want 0`, result, err)
+		t.Fatalf(`solvePartOne(input) = %v, %v; want 0.`, result, err)
+	}
+}
+
+func TestSolvePartTwo(t *testing.T) {
+	input := []string{"1721", "979", "366", "299", "675", "1456"}
+	var want int64 = 241861950
+	result, err := solvePartTwo(input)
+	if want != result || err != nil {
+		t.Fatalf(`solvePartTwo(input) = %v, %v; want %v.`, result, err, want)
+	}
+}
+
+func TestSolvePartTwoWithoutResult(t *testing.T) {
+	input := []string{"1721", "980", "366", "299", "675", "1456"}
+	result, err := solvePartTwo(input)
+	if result != 0 || err == nil {
+		t.Fatalf(`solvePartTwo(input) = %v, %v; want 0.`, result, err)
 	}
 }
