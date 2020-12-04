89fd30c25df73b4bd2af6e0271a847512cfa915e
natkam <natka_m@o2.pl>
2020-12-04 17:11:01 +0100

    [2020/d.4] Refactor the solutions

--- 2020/04_passports.py
+++ 2020/04_passports.py
@@ -1,22 +1,57 @@
 import re
 from typing import List
 
+PASSPORT_RULES = {
+    "byr": [
+        lambda x: len(x) == 4,
+        lambda x: 1920 <= int(x) <= 2002,
+    ],
+    "iyr": [
+        lambda x: len(x) == 4,
+        lambda x: 2010 <= int(x) <= 2020,
+    ],
+    "eyr": [
+        lambda x: len(x) == 4,
+        lambda x: 2020 <= int(x) <= 2030,
+    ],
+    "hgt": [
+        lambda x: re.match(r"^\d+(cm|in)$", x),
+        lambda x: (x[-2:] == "cm" and 150 <= int(x[:-2]) <= 193)
+                  or (x[-2:] == "in" and 59 <= int(x[:-2]) <= 76),
+    ],
+    "hcl": [
+        lambda x: x.startswith("#"),
+        lambda x: all(c.isdigit() or c in "abcdef" for c in x[1:]),
+    ],
+    "ecl": [
+        lambda x: x in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"],
+    ],
+    "pid": [
+        lambda x: len(x) == 9,
+        lambda x: x.isdigit(),
+    ],
+}
+
 
 def keys_are_valid(doc: List[str]) -> bool:
-    required_fields = [
-        "byr",
-        "iyr",
-        "eyr",
-        "hgt",
-        "hcl",
-        "ecl",
-        "pid",
-    ]
     doc_keys = [item[:3] for item in doc]
-    if all(field in doc_keys for field in required_fields):
+    if all(field in doc_keys for field in PASSPORT_RULES.keys()):
         return True
-    
+
     return False
+
+
+def values_are_valid(doc: List[str]) -> bool:
+    doc_fields = dict([field.split(":") for field in doc])
+
+    for required_field, validators in PASSPORT_RULES.items():
+        value = doc_fields.get(required_field)
+        if value is None:
+            return False  # This document lacks some required fields.
+        if not all(validate(value) for validate in validators):
+            return False  # Some field has an invalid value.
+
+    return True
 
 
 def solve_part_one():
@@ -28,48 +63,10 @@ def solve_part_one():
 
 
 def solve_part_two():
-    validators = {
-        "byr": [
-            lambda x: len(x) == 4,
-            lambda x: 1920 <= int(x) <= 2002,
-        ],
-        "iyr": [
-            lambda x: len(x) == 4,
-            lambda x: 2010 <= int(x) <= 2020,
-        ],
-        "eyr": [
-            lambda x: len(x) == 4,
-            lambda x: 2020 <= int(x) <= 2030,
-        ],
-        "hgt": [
-            lambda x: re.match(r"^\d+(cm|in)$", x),
-            lambda x: (x[-2:] == "cm" and 150 <= int(x[:-2]) <= 193)
-            or (x[-2:] == "in" and 59 <= int(x[:-2]) <= 76),
-        ],
-        "hcl": [
-            lambda x: x.startswith("#")
-            and all(c.isdigit() or c in "abcdef" for c in x[1:]),
-        ],
-        "ecl": [
-            lambda x: x in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"],
-        ],
-        "pid": [
-            lambda x: len(x) == 9 and x.isdigit(),
-        ],
-        "cid": [],
-    }
-
     valid_docs_count = 0
-
     for doc in DOCS:
-        if keys_are_valid(doc):
-            doc_fields = [field.split(":") for field in doc]
-            for field, value in doc_fields:
-                if not all(validate(value) for validate in validators[field]):
-                    break
-            else:
-                valid_docs_count += 1
-
+        if values_are_valid(doc):
+            valid_docs_count += 1
     return valid_docs_count
 
 
