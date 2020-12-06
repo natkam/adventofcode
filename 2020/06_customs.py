0f96cf8c97a2be6ee61ab667812986ebfe8b3a71
natkam <natka_m@o2.pl>
2020-12-06 17:55:04 +0100

    [2020/d.6] Alternative solution (sets, bitwise and) of pt.2

--- 2020/06_customs.py
+++ 2020/06_customs.py
@@ -1,3 +1,7 @@
+import functools
+import operator
+
+
 def solve_part_one():
     with open("06_input") as f:
         declarations = [
@@ -21,6 +25,20 @@ def solve_part_two():
     return common_answers
 
 
+def solve_part_two_2():
+    with open("06_input") as f:
+        declarations = [decl.split() for decl in f.read().split("\n\n")]
+
+    common_answers = 0
+
+    for group in declarations:
+        group_s = [set(person) for person in group]
+        common_answers += len(functools.reduce(operator.and_, group_s))
+
+    return common_answers
+
+
 if __name__ == "__main__":
     print(solve_part_one())
     print(solve_part_two())
+    print(solve_part_two_2())
