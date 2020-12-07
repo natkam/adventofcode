4ce800dd3d8b3f9302752c0591a312eb64b6d1f1
natkam <natka_m@o2.pl>
2020-12-07 21:15:16 +0100

    [2020/d.7] Correct solution of part 1 - outer bags

--- 2020/07_coloured_bags.py
+++ 2020/07_coloured_bags.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 from typing import Set
 
 from anytree import Node, findall_by_attr
@@ -28,15 +29,36 @@ def create_tree() -> Set[Node]:
     return roots
 
 
-def solve_part_one():
-    roots = create_tree()
+def get_containing_bags() -> defaultdict:
+    containing_bags = defaultdict(set)
+
+    for rule in rules:
+        parent_name = " ".join(rule.split()[:2])
+        children_names = re.findall(r"(?<= \d )\w+ \w+(?= bags*[,.])", rule)
+
+        for child in children_names:
+            containing_bags[child].add(parent_name)
+
+    return containing_bags
+
+
+def find_outer_bags_of(colour: str, containing_bags: defaultdict) -> Set[str]:
+    parents = containing_bags[colour]
+    outer_bags = set(parents)
+    
+    for parent in parents:
+        outer_bags |= find_outer_bags_of(parent, containing_bags)
+
+    return outer_bags
+
+
+def solve_part_one() -> int:
+    containing_bags = get_containing_bags()
     bag_colour = "shiny gold"
-    outer_bags = set()
-    for root in roots:
-        gold_nodes = findall_by_attr(root, bag_colour)
-        for node in gold_nodes:
-            outer_bags.update(node.ancestors)
+    outer_bags = find_outer_bags_of(bag_colour, containing_bags)
+
     return len(outer_bags)
+
 
 
 if __name__ == "__main__":
