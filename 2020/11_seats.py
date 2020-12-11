ff02f63934fe2d853665f66134e90764e7f687c3
natkam <natka_m@o2.pl>
2020-12-12 00:07:08 +0100

    [2020/d.11] Refactor the solutions - game of seats

--- 2020/11_seats.py
+++ 2020/11_seats.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from typing import Callable, List, Tuple
+from typing import Callable, List, Optional, Tuple
 
 
 class StableStateException(Exception):
@@ -7,12 +7,20 @@ class StableStateException(Exception):
 
 
 class Grid:
-    def __init__(self, seats: List[List[str]], tolerance_level: int):
-        """Tolerance level means how many neighbours can a person stand before they go away."""
+    def __init__(
+        self,
+        seats: List[List[str]],
+        tolerance_level: int,
+        max_dist: Optional[int] = None,
+    ):
+        """Tolerance level is how many neighbours makes a person leave their seat;
+        max_dist is how far can be the nearest seat to be relevant.
+        """
         self.seats = seats
         self.size = (len(seats[0]), len(seats))  # x, y
         self.previous_state: List[List[str]] = []
         self.tolerance = tolerance_level
+        self.max_dist = max_dist or self.size[0] + self.size[1]
         self.age = 0
 
     @property
@@ -29,6 +37,14 @@ class Grid:
 
         return False
 
+    def _is_floor(self, x: int, y: int) -> bool:
+        return self.previous_state[y][x] == "."
+
+    def _is_occupied(self, x: int, y: int) -> bool:
+        if self._is_floor(x, y):
+            raise ValueError(f"There's floor at ({x}, {y}) - it cannot be occupied")
+        return self.previous_state[y][x] == "#"
+
     def _count_occupied_adjacent_seats(self, x: int, y: int) -> int:
         occupied_adjacent_seats = 0
         nearest_seat_azimuths: List[Callable[[int], Tuple[int, int]]] = [
@@ -42,28 +58,29 @@ class Grid:
             lambda d: (x - d, y + d),
         ]
         for get_nearest_seat in nearest_seat_azimuths:
-            for dist in range(1, self.size[0] + self.size[1]):
+            for dist in range(1, self.max_dist + 1):
                 x_n, y_n = get_nearest_seat(dist)
                 if self._index_outside_gird(x_n, y_n):
                     break
-                if self.previous_state[y_n][x_n] != ".":
-                    if self.previous_state[y_n][x_n] == "#":
+                if not self._is_floor(x_n, y_n):
+                    if self._is_occupied(x_n, y_n):
                         occupied_adjacent_seats += 1
+
+                        # Optimisation - but very slight, ca. 1 s less on my machine
+                        if occupied_adjacent_seats >= self.tolerance:
+                            return occupied_adjacent_seats
                     break
 
         return occupied_adjacent_seats
 
     def _update_seat(self, x: int, y: int):
-        if self.seats[y][x] == ".":
+        if self._is_floor(x, y):
             return
 
         occupied_neighbours = self._count_occupied_adjacent_seats(x, y)
-        if self.previous_state[y][x] == "L" and not occupied_neighbours:
+        if not self._is_occupied(x, y) and not occupied_neighbours:
             self.seats[y][x] = "#"
-        elif (
-                self.previous_state[y][x] == "#"
-                and occupied_neighbours >= self.tolerance
-        ):
+        elif self._is_occupied(x, y) and occupied_neighbours >= self.tolerance:
             self.seats[y][x] = "L"
 
     def iterate(self):
@@ -77,14 +94,14 @@ class Grid:
             raise StableStateException("No seats change their state any more.")
 
         self.age += 1
-        # print(self, "\n")
 
     def __str__(self):
         return "\n".join("".join(self.seats[y]) for y in range(self.size[1]))
 
 
 def solve_part_one() -> int:
-    grid = Grid(deepcopy(seats), tolerance_level=4)
+    grid = Grid(deepcopy(seats), tolerance_level=4, max_dist=1)
+
     while True:
         try:
             grid.iterate()
@@ -96,14 +113,13 @@ def solve_part_one() -> int:
 
 def solve_part_two() -> int:
     grid = Grid(deepcopy(seats), tolerance_level=5)
-    # print(grid, "\n")
+
     while True:
         try:
             grid.iterate()
         except StableStateException:
             break
 
-    # print(grid.age)
     return grid.occupied_seats_count
 
 
@@ -111,5 +127,5 @@ if __name__ == "__main__":
     with open("11_input") as f:
         seats = [[seat for seat in row] for row in f.read().splitlines()]
 
-    # print(solve_part_one())
+    print(solve_part_one())
     print(solve_part_two())
