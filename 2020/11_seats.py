4fee84a84d8fc9e0e2f61a4515f7190b3c262da8
natkam <natka_m@o2.pl>
2020-12-11 21:27:14 +0100

    [2020/d.11] Solve pt.2 (Game of Seats)

--- 2020/11_seats.py
+++ 2020/11_seats.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from typing import List, Tuple
+from typing import Callable, List, Tuple
 
 
 class StableStateException(Exception):
@@ -7,10 +7,12 @@ class StableStateException(Exception):
 
 
 class Grid:
-    def __init__(self, seats: List[List[str]]):
+    def __init__(self, seats: List[List[str]], tolerance_level: int):
+        """Tolerance level means how many neighbours can a person stand before they go away."""
         self.seats = seats
         self.size = (len(seats[0]), len(seats))  # x, y
         self.previous_state: List[List[str]] = []
+        self.tolerance = tolerance_level
         self.age = 0
 
     @property
@@ -19,25 +21,32 @@ class Grid:
 
     def _count_occupied_adjacent_seats(self, x: int, y: int) -> int:
         occupied_adjacent_seats = 0
-        neighbours: List[Tuple[int, int]] = [
-            (x - 1, y),
-            (x - 1, y - 1),
-            (x, y - 1),
-            (x + 1, y - 1),
-            (x + 1, y),
-            (x + 1, y + 1),
-            (x, y + 1),
-            (x - 1, y + 1),
+        nearest_seat_azimuths: List[Callable[[int], Tuple[int, int]]] = [
+            lambda d: (x - d, y),
+            lambda d: (x - d, y - d),
+            lambda d: (x, y - d),
+            lambda d: (x + d, y - d),
+            lambda d: (x + d, y),
+            lambda d: (x + d, y + d),
+            lambda d: (x, y + d),
+            lambda d: (x - d, y + d),
         ]
-        for x_n, y_n in neighbours:
+        for direction in nearest_seat_azimuths:
+            dist = 1  # distance from the next seat
+            x_n, y_n = direction(dist)
             if x_n < 0 or y_n < 0:  # Cannot rely on IndexError here. Python <3
                 continue
             try:
-                occupied_adjacent_seats += (
-                    1 if self.previous_state[y_n][x_n] == "#" else 0
-                )
+                while self.previous_state[y_n][x_n] == ".":
+                    dist += 1
+                    x_n, y_n = direction(dist)
+                    if x_n < 0 or y_n < 0:  # Cannot rely on IndexError here.
+                        raise IndexError
             except IndexError:
-                pass
+                continue
+
+            if self.previous_state[y_n][x_n] == "#":
+                occupied_adjacent_seats += 1
 
         return occupied_adjacent_seats
 
@@ -54,7 +63,10 @@ class Grid:
                 occupied_neighbours = self._count_occupied_adjacent_seats(x, y)
                 if self.previous_state[y][x] == "L" and not occupied_neighbours:
                     self.seats[y][x] = "#"
-                elif self.previous_state[y][x] == "#" and occupied_neighbours >= 4:
+                elif (
+                    self.previous_state[y][x] == "#"
+                    and occupied_neighbours >= self.tolerance
+                ):
                     self.seats[y][x] = "L"
 
         if self.seats == self.previous_state:
@@ -68,7 +80,18 @@ class Grid:
 
 
 def solve_part_one() -> int:
-    grid = Grid(deepcopy(seats))
+    grid = Grid(deepcopy(seats), tolerance_level=4)
+    while True:
+        try:
+            grid.iterate()
+        except StableStateException:
+            break
+
+    return grid.occupied_seats_count
+
+
+def solve_part_two() -> int:
+    grid = Grid(deepcopy(seats), tolerance_level=5)
     # print(grid, "\n")
     while True:
         try:
@@ -84,4 +107,4 @@ if __name__ == "__main__":
     with open("11_input") as f:
         seats = [[seat for seat in row] for row in f.read().splitlines()]
 
-    print(solve_part_one())
+    print(solve_part_two())
