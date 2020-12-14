0b31157b1f8a579a5b8aaa0d3193e7e6adec4c70
natkam <natka_m@o2.pl>
2020-12-14 03:55:31 +0100

    [2020/d.12] Solve part 2 - ferry route with a waypoint

--- 2020/12_ferry.py
+++ 2020/12_ferry.py
@@ -29,9 +29,39 @@ def solve_part_one(instructions: List[Tuple[str, int]]) -> int:
     return abs(position["x"]) + abs(position["y"])
 
 
-if __name__ == '__main__':
+def solve_part_two(instructions: List[Tuple[str, int]]) -> int:
+    position = {"x": 0, "y": 0}
+    waypoint = {"x": 10, "y": 1}  # relative to the ship's position!
+
+    for action, val in instructions:
+        if action == "N":
+            waypoint["y"] += val
+        elif action == "S":
+            waypoint["y"] -= val
+        elif action == "E":
+            waypoint["x"] += val
+        elif action == "W":
+            waypoint["x"] -= val
+        elif action == "F":
+            position["x"] += val * waypoint["x"]
+            position["y"] += val * waypoint["y"]
+        else:
+            if val == 180:
+                waypoint["x"] -= 2 * waypoint["x"]
+                waypoint["y"] -= 2 * waypoint["y"]
+            elif (action == "R" and val == 90) or (action == "L" and val == 270):
+                waypoint["x"] = waypoint["y"]
+                waypoint["y"] = -waypoint["x"]
+            elif (action == "L" and val == 90) or (action == "R" and val == 270):
+                waypoint["x"] = -waypoint["y"]
+                waypoint["y"] = waypoint["x"]
+
+    return abs(position["x"]) + abs(position["y"])
+
+
+if __name__ == "__main__":
     with open("12_input") as f:
         instructions = [(instr[0], int(instr[1:])) for instr in f.read().splitlines()]
 
     print(solve_part_one(instructions))
-    # print(solve_part_two())
+    print(solve_part_two(instructions))
