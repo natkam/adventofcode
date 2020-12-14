872851dea818863b8aea5e2515c2fe6f1831b2fa
natkam <natka_m@o2.pl>
2020-12-14 02:15:37 +0100

    [2020/d.13] Formatting, comments - part 2

--- 2020/13_shuttle_bus.py
+++ 2020/13_shuttle_bus.py
@@ -11,43 +11,44 @@ def solve_part_one() -> int:
 
 
 def solve_part_two(ids: List[str]):
-    # The bus that's supposed to leave at time 0 (the timestamp we're looking for) is 19.
-    # Four of the buses will leave 19 minutes af the timestamp we're looking for (ts):
-    # 41, 29, 383 - and 19, obviously. Therefore the ts has to be a multiple of these
-    # four numbers product (8652353), minus 19.
+    """The ID of the bus that's supposed to leave at time `ts` (the timestamp we're
+    looking for) is 19.
+    Four of the buses will leave 19 minutes *after* the timestamp we're looking for:
+    41, 29, 383 - and 19, obviously. Therefore `ts` has to be a multiple of these four
+    numbers' product (8_652_353), minus 19."""
 
-    # ts is supposed to be bigger than 100_000_000_000_000, so let's start with:
-    import time
-    start = time.perf_counter()
-    ts = 8652353 * 11557549 - 19  # = 99999993762778
+    # import time
+    # start = time.perf_counter()
+
+    # `ts` is declared to be at least 100_000_000_000_000, so let's start with:
+    ts = 8_652_353 * 11_557_549 - 19  # = 99_999_993_762_778
     while True:
-        waiting_times = {bus_id: int(bus_id) - ts % int(bus_id) for bus_id in ids if bus_id != "x"}
-        # waiting_times = sorted(waiting_times.items(), key=operator.itemgetter(1))
+        waiting_times = {
+            bus_id: int(bus_id) - ts % int(bus_id) for bus_id in ids if bus_id != "x"
+        }
         for minute, bus_id in enumerate(ids):
             if bus_id == "x" or bus_id == ids[0]:
                 continue
-            if waiting_times[bus_id] != minute % int(bus_id) or (not minute % int(bus_id) and waiting_times[bus_id] == int(bus_id)):
-                # print(f"{ts=}, {minute=} ({minute % int(bus_id)}), {bus_id=}, ")
+            if waiting_times[bus_id] != minute % int(bus_id) or (
+                not minute % int(bus_id) and waiting_times[bus_id] == int(bus_id)
+            ):
                 break
         else:
             print(f"{ts=}, {waiting_times}")
             break
 
-
-        # print(waiting_times)
         ts += 8652353  # The product of 19, 41, 29 and 383
-        if ts > 1_000_000_000_000_000:
-            print("Exceeded 10^15, breaking")
-            return
-        if ts == 108_652_353_000_000:
-            print(f"{ts=} checkpoint: {time.perf_counter() - start} seconds")
-        if ts == 186_523_523_762_778:  # 10k rounds from the start
-            print(f"{ts=} checkpoint: {time.perf_counter() - start} seconds")
-        if ts == 619_141_173_762_778:  # 60k rounds
-            print(f"{ts=} checkpoint: {time.perf_counter() - start} seconds")
-        if ts == 965_235_293_762_778:  # 100k rounds
-            print(f"{ts=} checkpoint: {time.perf_counter() - start} seconds")
-    print(ids)
+        # if ts > 1_000_000_000_000_000:
+        #     print("Exceeded 10^15, breaking")
+        #     return
+        # if ts == 108_652_353_000_000:
+        #     print(f"{ts=} checkpoint: {time.perf_counter() - start} seconds")
+        # if ts == 186_523_523_762_778:  # 10k rounds from the start
+        #     print(f"{ts=} checkpoint: {time.perf_counter() - start} seconds")
+        # if ts == 619_141_173_762_778:  # 60k rounds
+        #     print(f"{ts=} checkpoint: {time.perf_counter() - start} seconds")
+        # if ts == 965_235_293_762_778:  # 100k rounds
+        #     print(f"{ts=} checkpoint: {time.perf_counter() - start} seconds")
     return ts
 
 
@@ -72,12 +73,11 @@ def test_part_two():
     # print(f"The test took {time.perf_counter() - start} seconds.")  # ~3 seconds
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     with open("13_input") as f:
         timestamp, all_ids = f.read().splitlines()
     ts = int(timestamp)
 
-    # print(solve_part_one())
+    print(solve_part_one())
     # test_part_two()
     print(solve_part_two(all_ids.split(",")))
