82b6c8286cd33e0cb244c33035f35bdcb42163ca
natkam <natka_m@o2.pl>
2020-12-14 18:57:20 +0100

    [2020/d.14] Refactor the solutions

--- 2020/14_bitmasks.py
+++ 2020/14_bitmasks.py
@@ -1,17 +1,15 @@
 from itertools import product
-from typing import List
+from typing import List, Tuple
 
 BIT_COUNT = 36
 
 
+def to_binary(number: int) -> str:
+    return f"{number:0{BIT_COUNT}b}"
+
+
 def apply_bitmask_to_values(val: str, mask: str) -> str:
-    masked_val = ""
-    for v, m in zip(val, mask):
-        if m == "X":
-            masked_val += v
-        else:
-            masked_val += m
-    return masked_val
+    return "".join(v if m == "X" else m for v, m in zip(val, mask))
 
 
 def solve_part_one() -> int:
@@ -22,47 +20,53 @@ def solve_part_one() -> int:
             bitmask = val
         else:
             address = action.strip("me[]")
-            masked_val = apply_bitmask_to_values(f"{int(val):0{BIT_COUNT}b}", bitmask)
+            masked_val = apply_bitmask_to_values(to_binary(int(val)), bitmask)
             memory[address] = int(masked_val, base=2)
 
     return sum(memory.values())
 
 
-def apply_bitmask_to_address(address: bytearray, mask: bytearray) -> List[int]:
-    floating_bits = []
+def apply_bitmask_to_address(
+    address: bytearray, mask: bytearray
+) -> Tuple[bytearray, List[int]]:
+    floating_indexes = []
     for i, a, m in zip(range(BIT_COUNT), address, mask):
         if m == ord("0"):
             continue
-        elif m == ord("1"):
-            address[i] = ord("1")
-        elif m == ord("X"):
-            address[i] = ord("X")
-            floating_bits.append(i)
+        else:
+            address[i] = m
+            if m == ord("X"):
+                floating_indexes.append(i)
+    return address, floating_indexes
 
+
+def get_floating_addresses(
+    address: bytearray, floating_indexes: List[int]
+) -> List[bytearray]:
     addresses = []
-    for bits in product("01", repeat=len(floating_bits)):
+    for bits in product("01", repeat=len(floating_indexes)):
         new_address = address[:]
-        for i, bit in zip(floating_bits, bits):
+        for i, bit in zip(floating_indexes, bits):
             new_address[i] = ord(bit)
-        addresses.append(int(new_address))
+        addresses.append(new_address)
 
     return addresses
 
 
 def solve_part_two() -> int:
+    """I used bytearrays instead of str, so as to be able to mutate the addresses."""
     bitmask = bytearray()
     memory = dict()
     for action, val in instructions:
         if action == "mask":
             bitmask = bytearray(val, "utf-8")
         else:
-            address = action.strip("me[]")
-            addresses = apply_bitmask_to_address(
-                bytearray(f"{int(address):0{BIT_COUNT}b}", "utf-8"), bitmask
-            )
+            byte_address = bytearray(to_binary(int(action.strip("me[]"))), "utf-8")
+            address, floating_indexes = apply_bitmask_to_address(byte_address, bitmask)
+            addresses = get_floating_addresses(address, floating_indexes)
 
             for addr in addresses:
-                memory[addr] = int(val)
+                memory[int(addr)] = int(val)  # Bytearray is unhashable.
 
     return sum(memory.values())
 
