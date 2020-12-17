307eb2e791a39f990ec38b1d56b3513e257c3e63
natkam <natka_m@o2.pl>
2020-12-17 03:55:03 +0100

    [2020/d.16] [WIP] Refactor the solutions

--- 2020/16_train_tickets.py
+++ 2020/16_train_tickets.py
@@ -5,25 +5,37 @@ from typing import Dict, List, Tuple
 import pandas as pd
 
 
-def get_input_one() -> Tuple[List[range], List[List[int]]]:
+def read_string_data() -> Tuple[str, str, str]:
     with open("16_input") as f:
         data = f.read()
 
-    rules, _, nearby_tickets = data.split("\n\n")
+    rules, my_ticket, nearby_tickets = data.split("\n\n")
+    return rules, my_ticket, nearby_tickets
 
-    parsed_rules = []
+
+def parse_ranges(valid_values: str) -> List[range]:
+    m = re.match(r"(\d+)-(\d+) or (\d+)-(\d+)", valid_values)
+    assert m is not None  # for mypy
+    start_1, end_1, start_2, end_2 = [int(val) for val in m.groups()]
+
+    return [range(start_1, end_1 + 1), range(start_2, end_2 + 1)]
+
+
+def get_input_one() -> Tuple[List[range], List[List[int]]]:
+    rules, _, nearby_tickets = read_string_data()
+
+    all_ranges = []
     for rule in rules.splitlines():
-        _, valid_values_str = rule.split(": ")
-        m = re.match(r"(\d+)-(\d+) or (\d+)-(\d+)", valid_values_str)
-        start_1, end_1, start_2, end_2 = [int(val) for val in m.groups()]
-        parsed_rules.extend([range(start_1, end_1 + 1), range(start_2, end_2 + 1)])
+        _, valid_values = rule.split(": ")
+        ranges = parse_ranges(valid_values)
+        all_ranges.extend(ranges)
 
     tickets = [
         [int(number) for number in ticket.split(",")]
         for ticket in nearby_tickets.splitlines()[1:]
     ]
 
-    return parsed_rules, tickets
+    return all_ranges, tickets
 
 
 def solve_part_one() -> int:
@@ -39,24 +51,20 @@ def solve_part_one() -> int:
     return error_rate
 
 
-def get_input_two() -> Tuple[Dict[str, List[range]], List[int], pd.DataFrame]:
-    with open("16_input") as f:
-        data = f.read()
-
-    rules, my_ticket, nearby_tickets = data.split("\n\n")
-
-    tmp_rules: Dict[str, str] = {
-        field: values
-        for field, values in [line.split(": ") for line in rules.splitlines()]
-    }
+def get_rules_input(rules: str) -> Dict[str, List[range]]:
     parsed_rules = dict()
 
-    for rule, values in tmp_rules.items():
-        m = re.match(r"(\d+)-(\d+) or (\d+)-(\d+)", values)
-        assert m is not None  # for mypy
-        start_1, end_1, start_2, end_2 = [int(val) for val in m.groups()]
-        parsed_rules[rule] = [range(start_1, end_1 + 1), range(start_2, end_2 + 1)]
+    for rule in rules.splitlines():
+        field, valid_values = rule.split(": ")
+        parsed_rules[field] = parse_ranges(valid_values)
 
+    return parsed_rules
+
+
+def get_input_two() -> Tuple[Dict[str, List[range]], List[int], pd.DataFrame]:
+    rules, my_ticket, nearby_tickets = read_string_data()
+
+    parsed_rules = get_rules_input(rules)
     all_ranges = reduce(lambda a, b: a + b, parsed_rules.values())
 
     my_ticket_numbers = [int(number) for number in re.findall(r"\d+", my_ticket)]
@@ -105,7 +113,6 @@ def solve_part_two() -> int:
         for field, val in field_indexes.items()
         if field.startswith("departure")
     ]
-    print(departure_fields)
 
     return reduce(lambda a, b: a * b, [my_ticket[i] for i in departure_fields])
 
