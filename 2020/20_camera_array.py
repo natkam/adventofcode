0b88de8d9806a9333e8e919e58969bdff91f2bd1
natkam <natka_m@o2.pl>
2020-12-21 01:29:52 +0100

    [2020/d.20] Further refactor pt.1 solution

--- 2020/20_camera_array.py
+++ 2020/20_camera_array.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Tuple
+from typing import List, Optional, Tuple
 
 import numpy as np
 
@@ -25,35 +25,68 @@ class Tile:
         return f"Tile id={self.id}"
 
     def __repr__(self):
-        return str(self)
+        return str(self)  # For debugging only, so whatever
 
 
-class Solution:
-    def __init__(self, img_side: int, start_tile: Tile):
+class Image:
+    def __init__(self, img_side: int):
         img_shape = (
             (img_side + 1) * 2 + 1,
             (img_side + 1) * 2 + 1,
         )  # Each tile is 10x10; added extra space on the edges to avoid IndexErrors.
-        self.img = np.empty((*img_shape, 10, 10), dtype=str)
-        self.tile_ids = np.zeros(img_shape, dtype=int)
+        self._img = np.empty((*img_shape, 10, 10), dtype=str)
+        self._tile_ids = np.zeros(img_shape, dtype=int)
+        self._img_middle = (img_side + 1, img_side + 1)
+        self.assembled_tiles: List[Tile] = []
 
-        img_middle = (img_side + 1, img_side + 1)
-        self.img[img_middle] = start_tile.data
-        start_tile.coords = img_middle
-        self.tile_ids[img_middle] = start_tile.id
+    # TODO: define __getattr__ etc.? Anyway we only want to manipulate `self._img`.
 
-    # TODO: define __getattr__ etc.? Anyway we only want to manipulate `self.img`.
+    def assemble(self, tiles) -> None:
+        start_tile = tiles.pop()
+        self._set_tile(self._img_middle, start_tile)
 
-    def matches(self, slot: Tuple[int, int], tile: Tile) -> bool:
+        while self.assembled_tiles:
+            assembled_tile = self.assembled_tiles.pop()
+
+            for slot in assembled_tile.get_neighbours():
+                checked_tiles = []
+
+                while tiles:
+                    tile = tiles.pop()
+                    matched = self._try_to_match_tile(slot, tile)
+                    if matched:
+                        break
+                    checked_tiles.append(tile)
+
+                tiles += checked_tiles
+
+    def _set_tile(self, slot: Tuple[int, int], tile: Tile) -> None:
+        self._img[slot] = tile.data
+        self._tile_ids[slot] = tile.id
+        tile.coords = slot
+        self.assembled_tiles.append(tile)
+
+    def _try_to_match_tile(self, slot: Tuple[int, int], tile: Tile) -> bool:
+        for i in range(8):
+            if i == 4:  # Transpose the array after the 4th rotation.
+                tile.data = tile.data.T
+            tile.data = np.rot90(tile.data)
+            if self._matches(slot, tile):
+                self._set_tile(slot, tile)
+                return True
+
+        return False
+
+    def _matches(self, slot: Tuple[int, int], tile: Tile) -> bool:
         y, x = slot
-        right_tile = self.img[y, x + 1]
-        right_tile_id = self.tile_ids[y, x + 1]
-        bottom_tile = self.img[y + 1, x]
-        bottom_tile_id = self.tile_ids[y + 1, x]
-        left_tile = self.img[y, x - 1]
-        left_tile_id = self.tile_ids[y, x - 1]
-        up_tile = self.img[y - 1, x]
-        up_tile_id = self.tile_ids[y - 1, x]
+        right_tile = self._img[y, x + 1]
+        right_tile_id = self._tile_ids[y, x + 1]
+        bottom_tile = self._img[y + 1, x]
+        bottom_tile_id = self._tile_ids[y + 1, x]
+        left_tile = self._img[y, x - 1]
+        left_tile_id = self._tile_ids[y, x - 1]
+        up_tile = self._img[y - 1, x]
+        up_tile_id = self._tile_ids[y - 1, x]
 
         if right_tile_id != 0 and not (tile.data[:, -1] == right_tile[:, 0]).all():
             return False
@@ -66,15 +99,15 @@ class Solution:
 
         return True
 
-    def get_corner_tiles(self):
-        min_y, min_x = np.min(np.argwhere(self.tile_ids), axis=0)
-        max_y, max_x = np.max(np.argwhere(self.tile_ids), axis=0)
+    def get_corners_product(self):
+        min_y, min_x = np.min(np.argwhere(self._tile_ids), axis=0)
+        max_y, max_x = np.max(np.argwhere(self._tile_ids), axis=0)
 
         return (
-            self.tile_ids[min_y, min_x]
-            * self.tile_ids[min_y, max_x]
-            * self.tile_ids[max_y, min_x]
-            * self.tile_ids[max_y, max_x]
+            self._tile_ids[min_y, min_x]
+            * self._tile_ids[min_y, max_x]
+            * self._tile_ids[max_y, min_x]
+            * self._tile_ids[max_y, max_x]
         )
 
 
@@ -89,46 +122,15 @@ def get_input_from_file() -> List[Tile]:
     return [Tile(id_, data) for id_, data in tiles_dict.items()]
 
 
-def assemble(tiles):
-    img_side = int(len(tiles) ** 0.5)
-    start_tile = tiles.pop()
-    sol = Solution(img_side, start_tile)
-    assembled_tiles = [start_tile]
-    while assembled_tiles:
-        added_tile = assembled_tiles.pop()
-
-        for slot in added_tile.get_neighbours():
-            checked_tiles = []
-
-            while tiles:
-                tile = tiles.pop()
-                for i in range(8):
-                    if i == 4:  # Transpose the array after the 4th rotation.
-                        tile.data = tile.data.T
-                    tile.data = np.rot90(tile.data)
-                    if sol.matches(slot, tile):
-                        sol.img[slot] = tile.data
-                        sol.tile_ids[slot] = tile.id
-                        tile.coords = slot
-                        assembled_tiles.append(tile)
-                        break
-                else:
-                    checked_tiles.append(tile)
-                    continue
-
-                break
-
-            tiles = tiles + checked_tiles
-    return sol
-
-
 def solve_part_one():
     tiles = get_input_from_file()
 
-    sol = assemble(tiles)
+    img_side = int(len(tiles) ** 0.5)
+    solution = Image(img_side)
+    solution.assemble(tiles)
+    # print(solution.tile_ids)
 
-    # print(sol.tile_ids)
-    return sol.get_corner_tiles()
+    return solution.get_corners_product()
 
 
 if __name__ == "__main__":
