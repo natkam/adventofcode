38cd64dcf10084aaf611a0c6865192bea1b94aed
natkam <natka_m@o2.pl>
2020-12-23 00:39:14 +0100

    [2020/d.18] Solve pt.1 - operator precedence

--- 2020/18_operator_precedence.py
+++ 2020/18_operator_precedence.py
@@ -2,16 +2,39 @@ import ast
 from typing import List
 
 
+def test_solve_part_one():
+    expressions = [
+        ("1 + 2 * 3 + 4 * 5 + 6", 71),
+        ("1 + (2 * 3) + (4 * (5 + 6))", 51),
+        ("2 * 3 + (4 * 5)", 26),
+        ("5 + (8 * 3 + 9 + 3 * 4 * 3)", 437),
+        ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240),
+        ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632),
+    ]
 
-def evaluate(line: str) -> int:
+    for expression, expected in expressions:
+        result = evaluate(expression)
+        assert result == expected, f"Expected {expected}, got {result}"
+
+
+def evaluate(expression: str) -> int:
     # Replace '*' with '-' ('-' and '+' have equal precedence)
     # Let Python create an ast tree
     # Replace 'ast.Sub' with 'ast.Mult' (͡° ͜ʖ͡°)
-    #  ast.walk? or use a NodeTransformer?
-    # Compile, evaluate
-    line = line.replace("*", "-")
-    p = ast.parse(line)
-    ...
+    # Compile, evaluate - voilà!
+
+    expr_sub = expression.replace("*", "-")
+    p = ast.parse(expr_sub)
+
+    for node in ast.walk(p):
+        if hasattr(node, 'body'):
+            node = node.body[0].value
+        if isinstance(node, ast.BinOp) and isinstance(node.op, ast.Sub):
+            node.op = ast.Mult()
+
+    e = ast.Expression(p.body[0].value)
+
+    return eval(compile(e, "<string>", "eval"))
 
 
 def solve_part_one(homework: List[str]) -> int:
