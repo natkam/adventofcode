8aed8a57d985b7ed12a2970af319efbaa9084147
natkam <natka_m@o2.pl>
2020-12-23 22:49:59 +0100

    [2020/d.18] Finish refactoring the solution - operator precedence

--- 2020/18_operator_precedence.py
+++ 2020/18_operator_precedence.py
@@ -12,10 +12,8 @@ def test_solve_part_one():
         ("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 12240),
         ("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 13632),
     ]
-    s = SolvesPartOne()
-
     for expression, expected in expressions:
-        result = s.evaluate(expression)
+        result = SolvesPartOne.evaluate(expression)
         assert result == expected, f"Expected {expected}, got {result}"
 
 
@@ -25,10 +23,8 @@ def test_solve_part_two():
         ("1 + (2 * 3) + (4 * (5 + 6))", 51),
         ("2 * 3 + (4 * 5)", 46),
     ]
-    s = SolvesPartTwo()
-
     for expression, expected in expressions:
-        result = s.evaluate(expression)
+        result = SolvesPartTwo.evaluate(expression)
         assert result == expected, f"Expected {expected}, got {result}"
 
 
@@ -36,38 +32,42 @@ class OperatorPrecedenceChanger(abc.ABC):
     swapped_signs: Dict[str, Union[int, str, None]]
     ops_to_swap: Dict[Type[ast.operator], Type[ast.operator]]
 
-    def evaluate(self, expression: str) -> int:
-        expr_swapped = expression.translate(str.maketrans(self.swapped_signs))
+    def __init__(self, homework: List[str]):
+        self.homework = homework
+
+    @classmethod
+    def evaluate(cls, expression: str) -> int:
+        """Evaluates the expression string with overridden operator precedence.
+
+        How the operator precedence is overriden:
+        1. Replace the operator signs in `expressions` according to `cls.swapped_signs`
+        translation table.
+        2. Let Python build an AST, in compliance with overriden operator precedences.
+        3. Traverses the tree, replacing operator instances back to what they would be
+        if the original `expression` was parsed.
+        4. Compiles the expression and evaluates it, returning an integer (the result).
+        """
+        expr_swapped = expression.translate(str.maketrans(cls.swapped_signs))
         p = ast.parse(expr_swapped)
 
         for node in ast.walk(p):
             if isinstance(node, ast.BinOp) and isinstance(
-                node.op, tuple(self.ops_to_swap)
+                node.op, tuple(cls.ops_to_swap)
             ):
-                node.op = self.ops_to_swap[type(node.op)]()
+                node.op = cls.ops_to_swap[type(node.op)]()
 
         expr_node = cast(ast.Expr, p.body[0])
         e = ast.Expression(expr_node.value)
 
         return eval(compile(e, "<string>", "eval"))
 
+    def solve(self) -> int:
+        return sum(self.evaluate(expression) for expression in self.homework)
+
 
 class SolvesPartOne(OperatorPrecedenceChanger):
     swapped_signs = {"*": "-"}
     ops_to_swap = {ast.Sub: ast.Mult}
-
-# def solve_part_one(homework: List[str]) -> int:
-#     # Replace '*' with '-' ('-' and '+' have equal precedence)
-#     # Let Python create an ast tree
-#     # Replace 'ast.Sub' with 'ast.Mult' (͡° ͜ʖ͡°)
-#     # Compile, evaluate - voilà!
-#
-#     # swapped_signs: Dict[str, Union[int, str, None]] = {"*": "-"}
-#     # ops_to_swap: Dict[Type[ast.operator], Type[ast.operator]] = {ast.Sub: ast.Mult}
-#     # s = OperatorPrecedenceChanger(swapped_signs, ops_to_swap)
-#     s = SolvesPartOne()
-#
-#     return sum(s.evaluate(expression) for expression in homework)
 
 
 class SolvesPartTwo(OperatorPrecedenceChanger):
@@ -75,26 +75,11 @@ class SolvesPartTwo(OperatorPrecedenceChanger):
     ops_to_swap = {ast.Add: ast.Mult, ast.Mult: ast.Add}
 
 
-# def solve_part_two(homework: List[str]) -> int:
-#     # swapped_signs: Dict[str, Union[int, str, None]] = {"+": "*", "*": "+"}
-#     # ops_to_swap = {ast.Add: ast.Mult, ast.Mult: ast.Add}
-#     # s = OperatorPrecedenceChanger(swapped_signs, ops_to_swap)
-#     s = SolvesPartTwo()
-#
-#     return sum(s.evaluate(expression) for expression in homework)
-
-
-def solve(s: OperatorPrecedenceChanger, homework: List[str]) -> int:
-    return sum(s.evaluate(expression) for expression in homework)
-
-
 if __name__ == "__main__":
     with open("18_input") as f:
         homework = f.read().splitlines()
 
-    # test_solve_part_one()
-    # print(solve_part_one(homework))
-    # test_solve_part_two()
-    # print(solve_part_two(homework))
-    print(solve(SolvesPartOne(), homework))
-    print(solve(SolvesPartTwo(), homework))
+    test_solve_part_one()
+    test_solve_part_two()
+    print(SolvesPartOne(homework).solve())
+    print(SolvesPartTwo(homework).solve())
