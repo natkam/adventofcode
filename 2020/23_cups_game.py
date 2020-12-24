c47f42b8e4a09b6aaf2e05391842760eca88fc01
natkam <natka_m@o2.pl>
2020-12-24 23:45:17 +0100

    [2020/d.23] Solve pt.1 - crab's cups game with 9 cups

--- 2020/23_cups_game.py
+++ 2020/23_cups_game.py
@@ -3,24 +3,35 @@ from typing import List, Tuple
 
 
 def test_solve_part_one():
-    input_ = "32415"
+    input_ = "389125467"
     result = solve_part_one(input_, moves=10)
     assert result == "92658374", f"Expected 92658374, got {result}."
     result = solve_part_one(input_)
     assert result == "67384529", f"Expected 67384529, got {result}."
 
 
-def make_a_move(labels: List[int], current_cup: int) -> Tuple[List[int], int]:
-    ...
+def make_a_move(labels: deque) -> deque:
+    current_cup = labels.popleft()
+    # print(f"cups: {labels} (current: {current_cup})")
+    pick_up = [labels.popleft() for _ in range(3)]
+    # print(f"pick up: {pick_up}")
+    destination = max([n for n in labels if n < current_cup] or labels)
+    # print(f"destination: {destination}")
+    labels.appendleft(current_cup)
+    labels.rotate(-(labels.index(destination) + 1))  # Destination is at the end now.
+    labels.extend(pick_up)
+
+    labels.rotate(-(labels.index(current_cup) + 1))
+
+    return labels
 
 
 def solve_part_one(input_: str, moves: int = 100) -> str:
     labels = deque([int(i) for i in input_], maxlen=len(input_))
-    current_cup = deque[0]  # maybe this is not needed, just rotate the deque?
 
-    for _ in range(moves):
-        print(f"-- move {_} --")
-        labels, current_cup = make_a_move(labels, current_cup)
+    for _ in range(1, moves + 1):
+        # print(f"\n-- move {_} --")
+        labels = make_a_move(labels)
 
     labels.rotate(-labels.index(1))
     labels.popleft()
@@ -28,5 +39,10 @@ def solve_part_one(input_: str, moves: int = 100) -> str:
     return "".join(str(i) for i in labels)
 
 
+# def solve_part_two(input_: str, moves: int = 100) -> str:
+#     labels = deque([int(i) for i in input_], maxlen=len(input_))
+
+
 if __name__ == '__main__':
+    test_solve_part_one()
     print(solve_part_one("871369452"))
