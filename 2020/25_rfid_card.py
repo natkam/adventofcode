03b6d07bbf57c1c0e28cfcd0c6c082045e491cb2
natkam <natka_m@o2.pl>
2020-12-25 14:46:32 +0100

    [2020/d.25] Refactor pt.1 solution

--- 2020/25_rfid_card.py
+++ 2020/25_rfid_card.py
@@ -1,34 +1,42 @@
+from typing import Tuple
+
+DIVISOR = 20201227
+
+
 def test_solve_part_one():
     card_pubkey, door_pubkey = 5764801, 17807724
     result = solve_part_one(card_pubkey, door_pubkey)
     assert result == 14897079, f"Expected 14897079, got {result}"
 
 
-def solve_part_one(card_pubkey: int, door_pubkey: int) -> int:
-    subject_number = 7
-    value = 1
-    divisor = 20201227
-    loop_size = 0
+def transform(value: int, subject_number: int = 7) -> int:
+    value *= subject_number
+    return value % DIVISOR
 
+
+def find_loop_sizes(card_pubkey: int, door_pubkey: int) -> Tuple[int, int, int]:
+    value = 1
+    loop_size = 0
     card_loop_size = door_loop_size = 0
 
     while True:
         loop_size += 1
-        value *= subject_number
-        value %= divisor
+        value = transform(value)
         if value == card_pubkey:
             card_loop_size = loop_size
         if value == door_pubkey:
             door_loop_size = loop_size
         if card_loop_size and door_loop_size:
-            break
+            return card_loop_size, door_loop_size, value
 
-    new_loop_size = min(card_loop_size, door_loop_size)
-    subject_number = value
+
+def solve_part_one(card_pubkey: int, door_pubkey: int) -> int:
+    card_loop_size, door_loop_size, value = find_loop_sizes(card_pubkey, door_pubkey)
+
+    new_subject_number = value
     value = 1
-    for _ in range(new_loop_size):
-        value *= subject_number
-        value %= divisor
+    for _ in range(min(card_loop_size, door_loop_size)):
+        value = transform(value, new_subject_number)
 
     return value
 
