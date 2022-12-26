46740c7db9c1f45bbef6227a3418165ddafb12a4
Natalia Maniakowska <nmaniakowska@ubimet.com>
2022-12-26 18:37:51 +0100

    [2022/8] Solve pt.2

--- 2022/08_treehouse.py
+++ 2022/08_treehouse.py
@@ -30,7 +30,47 @@ def part_two():
         data = f.read().splitlines()
 
     data = [[int(tree) for tree in row] for row in data]
-    return
+    scenic_score = 0
+    for y in range(len(data)):
+        row = data[y]
+        for x in range(len(row)):
+            west = 1
+            while True:
+                if (west_i := x - west) < 0:
+                    west -= 1
+                    break
+                if row[west_i] >= row[x]:
+                    break
+                west += 1
+            east = 1
+            while True:
+                if (east_i := x + east) >= len(row):
+                    east -= 1
+                    break
+                if row[east_i] >= row[x]:
+                    break
+                east += 1
+            north = 1
+            while True:
+                if (north_i := y - north) < 0:
+                    north -= 1
+                    break
+                if data[north_i][x] >= row[x]:
+                    break
+                north += 1
+            south = 1
+            while True:
+                if (south_i := y + south) >= len(row):
+                    south -= 1
+                    break
+                if data[south_i][x] >= row[x]:
+                    break
+                south += 1
+            scenic_score = max(scenic_score, west * east * north * south)
+            if scenic_score == 199272:
+                breakpoint()
+
+    return scenic_score
 
 
 if __name__ == "__main__":
