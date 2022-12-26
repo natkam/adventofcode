767fedca7610f7a61a59b61fe4faf6c0a59f879d
Natalia Maniakowska <nmaniakowska@ubimet.com>
2022-12-26 16:28:10 +0100

    [2022/7] Refactor pt.1 + solve pt.2

--- 2022/07_directories.py
+++ 2022/07_directories.py
@@ -1,11 +1,12 @@
+import uuid
 from typing import Dict
 
 dir_sizes: Dict[str, int] = {}
-sum_below_threshold: int = 0
+# sum_below_threshold: int = 0
 
 
 def _get_dir_size(data):
-    global dir_sizes, sum_below_threshold
+    global dir_sizes
     i = 0
     size = 0
     while True:
@@ -18,13 +19,16 @@ def _get_dir_size(data):
                 i += 1
                 return i, size
             case ["$", "cd", subdir]:
+                if subdir in dir_sizes:
+                    # There are multiple subdirs with the same name, so just
+                    # adding their size to the dict doesn't work - some of them
+                    # would be overwritten! We add unique uuid to the name:
+                    subdir = f"{subdir}_{uuid.uuid4()}"
+
                 jump, sub_size = _get_dir_size(data[i + 1 :])
-                # Caution: there are multiple subdirs with the same name, so
-                # just adding their size to the dict doesn't work - some of them
-                # are overwritten!
-                # dir_sizes[subdir] = sub_size
-                if sub_size < 100_000:
-                    sum_below_threshold += sub_size
+                dir_sizes[subdir] = sub_size
+                # if sub_size < 100_000:
+                #     sum_below_threshold += sub_size
                 size += sub_size
                 i += jump
             case ["$", "ls"]:
@@ -42,15 +46,22 @@ def part_one():
     with open("07_input") as f:
         data = f.read().splitlines()
 
-    _, total_size = _get_dir_size(data)
-    # total_size_below_threshold = sum(size for size in dir_sizes.values() if size <= 100_000)
+    _get_dir_size(data)
+    threshold = 100_000
+    sum_below_threshold = sum(size for size in dir_sizes.values() if size <= threshold)
 
     return sum_below_threshold
 
 
 def part_two():
-    with open("07_input_test") as f:
-        data = f.read().splitlines()
+    disk_size = 70_000_000
+    necessary_free_space = 30_000_000
+
+    global dir_sizes
+    total_size = dir_sizes["/"]  # 48_044_502
+    min_to_release = necessary_free_space - (disk_size - total_size)
+
+    return min(v for v in dir_sizes.values() if v >= min_to_release)
 
 
 if __name__ == "__main__":
