ce490bc4a8f1c90cef7fea156b54b03966b354ee
natkam <3858688+natkam@users.noreply.github.com>
2024-12-30 21:31:57 +0100

    [2024/04] Solve pt. 2

--- 2024_haskell/04Xmas.hs
+++ 2024_haskell/04Xmas.hs
@@ -21,6 +21,15 @@ main = do
   let result1 = lr + rl + ud + du + rd + lu + ru + ld
   print result1
 
+  -- positions of the 'X'-s: left, right, bottom, top
+  let left = length (cross rows)
+  let right = length (cross (map reverse rows))
+  let top = length ((cross . transpose) rows)
+  let bottom = length ((cross . transpose) (reverse rows))
+
+  let result2 = left + right + top + bottom
+  print result2
+
 fromRow :: [Char] -> [Int]
 fromRow xs =
   [ i
@@ -37,4 +46,16 @@ diag rows =
         && 'M' == (rows !! (j + 1)) !! (i + 1)
         && 'A' == (rows !! (j + 2)) !! (i + 2)
         && 'S' == (rows !! (j + 3)) !! (i + 3)
+  ]
+
+cross :: [[Char]] -> [(Int, Int)]
+cross rows =
+  [ (i, j)
+    | i <- [0 .. length rows - 3],
+      j <- [0 .. length (rows !! 0) - 3],
+      'M' == (rows !! j) !! i
+        && 'M' == (rows !! (j + 2)) !! i
+        && 'A' == (rows !! (j + 1)) !! (i + 1)
+        && 'S' == (rows !! j) !! (i + 2)
+        && 'S' == (rows !! (j + 2)) !! (i + 2)
   ]
