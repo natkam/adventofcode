5bbc4519670ae008de9b855b661054e1623cf89e
natkam <3858688+natkam@users.noreply.github.com>
2024-12-30 22:08:20 +0100

    [2024/04] Refactor the solutions

--- 2024_haskell/04Xmas.hs
+++ 2024_haskell/04Xmas.hs
@@ -8,54 +8,58 @@ main = do
   let rows = lines input
 
   -- rows left-right & right-left, cols up-down & down-up;
-  let lr = length (concatMap fromRow rows)
-  let rl = length (concatMap (fromRow . reverse) rows)
-  let ud = length (concatMap fromRow (transpose rows))
-  let du = length (concatMap (fromRow . reverse) (transpose rows))
+  let lr = fromRows rows
+  let rl = fromRows (map reverse rows)
+  let ud = fromRows (transpose rows)
+  let du = fromRows ((transpose . reverse) rows)
   -- diagonal: right-down, left-up, right-up, left-down
-  let rd = length (diag rows)
-  let lu = length (diag (reverse rows))
-  let ru = length (diag (map reverse rows))
-  let ld = length (diag (reverse (map reverse rows)))
+  let rd = diag rows
+  let lu = diag (reverse rows)
+  let ru = diag (map reverse rows)
+  let ld = diag (reverse (map reverse rows))
 
   let result1 = lr + rl + ud + du + rd + lu + ru + ld
   print result1
 
-  -- positions of the 'X'-s: left, right, bottom, top
-  let left = length (cross rows)
-  let right = length (cross (map reverse rows))
-  let top = length ((cross . transpose) rows)
-  let bottom = length ((cross . transpose) (reverse rows))
+  -- positions of the 'M'-s: left, right, bottom, top
+  let left = crosses rows
+  let right = crosses (map reverse rows)
+  let top = crosses (transpose rows)
+  let bottom = crosses ((transpose . reverse) rows)
 
   let result2 = left + right + top + bottom
   print result2
 
-fromRow :: [Char] -> [Int]
-fromRow xs =
-  [ i
-    | i <- [0 .. length xs - 4],
-      "XMAS" == map (xs !!) [i .. i + 3]
-  ]
+fromRows :: [[Char]] -> Int
+fromRows rows =
+  length
+    [ (i, j)
+      | i <- [0 .. length rows - 4],
+        j <- [0 .. length (rows !! 0) - 1],
+        "XMAS" == map ((rows !! j) !!) [i .. i + 3]
+    ]
 
-diag :: [[Char]] -> [(Int, Int)]
+diag :: [[Char]] -> Int
 diag rows =
-  [ (i, j)
-    | j <- [0 .. length rows - 4],
-      i <- [0 .. length (rows !! 0) - 4],
-      'X' == (rows !! j) !! i
-        && 'M' == (rows !! (j + 1)) !! (i + 1)
-        && 'A' == (rows !! (j + 2)) !! (i + 2)
-        && 'S' == (rows !! (j + 3)) !! (i + 3)
-  ]
+  length
+    [ (i, j)
+      | i <- [0 .. length rows - 4],
+        j <- [0 .. length (rows !! 0) - 4],
+        'X' == (rows !! j) !! i
+          && 'M' == (rows !! (j + 1)) !! (i + 1)
+          && 'A' == (rows !! (j + 2)) !! (i + 2)
+          && 'S' == (rows !! (j + 3)) !! (i + 3)
+    ]
 
-cross :: [[Char]] -> [(Int, Int)]
-cross rows =
-  [ (i, j)
-    | i <- [0 .. length rows - 3],
-      j <- [0 .. length (rows !! 0) - 3],
-      'M' == (rows !! j) !! i
-        && 'M' == (rows !! (j + 2)) !! i
-        && 'A' == (rows !! (j + 1)) !! (i + 1)
-        && 'S' == (rows !! j) !! (i + 2)
-        && 'S' == (rows !! (j + 2)) !! (i + 2)
-  ]
+crosses :: [[Char]] -> Int
+crosses rows =
+  length
+    [ (i, j)
+      | i <- [0 .. length rows - 3],
+        j <- [0 .. length (rows !! 0) - 3],
+        'M' == (rows !! j) !! i
+          && 'M' == (rows !! (j + 2)) !! i
+          && 'A' == (rows !! (j + 1)) !! (i + 1)
+          && 'S' == (rows !! j) !! (i + 2)
+          && 'S' == (rows !! (j + 2)) !! (i + 2)
+    ]
